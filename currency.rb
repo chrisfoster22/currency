d17ecfda8c787eb9ba76003c230f77c215bc3138
Chris Foster <fosterchris22@gmail.com>
2015-01-20 15:45:49 -0500

    Test 3 passing

--- currency.rb
+++ currency.rb
@@ -1,9 +1,19 @@
 class Currency
-
+  attr_accessor :amount, :code
   def initialize(amount, code)
-    amount = @amount
-    code = @code
+    @amount = amount
+    @code = code
   end
 
+  def ==(another_currency)
+    self.code == (another_currency.code)
+    self.amount == (another_currency.amount)
+  end
+
+  def +(another_currency)
+    if self.code == another_currency.code
+      self.amount + another_currency.amount
+    end
+  end
 
 end
